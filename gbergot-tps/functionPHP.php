da25357b150895bc3d10bdac93b55757abe1db31
BERGOT Gaëtan <bergot.gaetan01@gmail.com>
2017-03-03 11:35:30 +0100

    Update functionPHP.php

--- gbergot-tps/functionPHP.php
+++ gbergot-tps/functionPHP.php
@@ -2,9 +2,21 @@
 
 //Déclaration d'une fonction
 function titre($titre,$niveau=1){
-  echo <h{$niveau}>{$titre}</h{$niveau}>;
-    
+  echo <h{$niveau}>{$titre}</h{$niveau}>;   
 }
+
+//
+function paragraphe($texte,$element="div"){
+  
+}
+
+//
+function paragrapheAvecTitre($titre,$texte="",$niveau=1,$element="div"){
+  
+}
+
+
+
 
 //Appel fonction
 titre("Bonjour",1);
